23c927c11b377bb9385c6846ec5b05e13b22e866
NeoD <flesakov.nik09@gmail.com>
2019-10-14 23:46:18 +0500

    Исправлена глупая ошибка с индексацией массива.

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ resolutions = ["64b", "128b", "256b", "512b"]
 errors = 0
 
 while start <= end or errors != 1000:
-    p = requests.get("https://vk.com/sticker/1-{0}-{1}-9".format(start, resolutions[resolution]))
+    p = requests.get("https://vk.com/sticker/1-{0}-{1}-9".format(start, resolutions[resolution - 1]))
     if p.status_code == 404:
         print(str(start) + " skip. Have an error")
         start += 1
         errors += 1
         continue
-    out = open("stickers/" + str(start) + "-{0}.png".format(resolutions[resolution]), "wb")
+    out = open("stickers/64x64/" + str(start) + "-{0}.png".format(resolutions[resolution - 1]), "wb")
     out.write(p.content)
     out.close()
     print(str(start) + " downloaded")
