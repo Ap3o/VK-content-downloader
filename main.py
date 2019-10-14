cd481fe35c77ed02884029914c877e4a07e2851c
NeoD <flesakov.nik09@gmail.com>
2019-10-14 23:39:49 +0500

    Сделан счетчик ошибок.
    Если ошибок было более 1.000(404 page), то работы программы останавливается.

--- main.py
+++ main.py
@@ -8,14 +8,20 @@ resolution = int(input("1)64x64\n2)128x128\n3)256x256\n4)512x512\nКачеств
 
 resolutions = ["64b", "128b", "256b", "512b"]
 
-while start <= end:
+errors = 0
+
+while start <= end or errors != 1000:
     p = requests.get("https://vk.com/sticker/1-{0}-{1}-9".format(start, resolutions[resolution]))
     if p.status_code == 404:
         print(str(start) + " skip. Have an error")
         start += 1
+        errors += 1
         continue
     out = open("stickers/" + str(start) + "-{0}.png".format(resolutions[resolution]), "wb")
     out.write(p.content)
     out.close()
     print(str(start) + " downloaded")
     start += 1
+
+print("Скачано стикеров", end - start, sep=' ')
+print("Ошибок при скачивании:", errors, sep='')
